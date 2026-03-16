323649a44f9aa018f5e87d01fa06293751da650c
Jonah Kimani <jkim.kimata@gmail.com>
2026-03-16 14:46:41 +0300

    cors additions

--- backend/src/server.js
+++ backend/src/server.js
@@ -41,7 +41,7 @@ app.use(helmet({
   credentials: true,
 })); */
 app.use(cors({
-  origin: ['https://rms-frontend-uo3t.onrender.com', 'http://localhost:5173'],
+  origin: ['https://rms-frontend-uo3t.onrender.com', 'https://property-rental-ms.vercel.app/','http://localhost:5173'],
   credentials: true,
 }));
 
